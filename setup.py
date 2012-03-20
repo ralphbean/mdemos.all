0129693dbfc0ddb024dc39070d7aaac50edd07ab
Ralph Bean <rbean@redhat.com>
2012-03-20 15:22:20 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name="mdemos.all",
+    version="0.1.0b",
+    url="http://moksha.fedorahosted.org",
+    description="Meta-package to install all Moksha demo apps",
+    long_description="",
+    author="Ralph Bean",
+    author_email="rbean@redhat.com",
+    license="ASL 2.0",
+    packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        "mdemos.menus",
+        "mdemos.metrics",
+        "mdemos.chat",
+        "mdemos.feeds",  # <-- this one doesn't actually work.
+    ],
+)
